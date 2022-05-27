e00215a283ff2cee38363156a8d610859cd2989e
michaelteye <michaelteyegyemah@gmail.com>
2022-05-27 20:01:14 +0000

    The Project-Planer

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav class="main-nav">
+      <!-- <router-link :to="{ name: 'home', params: {}}">Projects</router-link> -->
+      <!-- <router-link :to="{ name: 'addProject', params: { }}">Add a new project</router-link> -->
+
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
